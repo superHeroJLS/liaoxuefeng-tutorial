d3aec68b934b4b548cb0d78276fbfa422ad9271b
jiangls <1257892551@qq.com>
2020-06-13 21:40:54 +0800

    fix(com.javabase):modify package name

---
+++ src/com/javabase/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.javabase;
+
+public class HelloWorld {
+    
+    private String name;
+    
+	public HelloWorld(String name) {
+        super();
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+		new HelloWorld("jianglinsheng").test();
+	}
+	
+	public void test() {
+	    System.out.println(HelloWorld.this instanceof HelloWorld);
+	    System.out.println(HelloWorld.this.name);
+	}
+	
+	
+}
--- src/java/base/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package java.base;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
