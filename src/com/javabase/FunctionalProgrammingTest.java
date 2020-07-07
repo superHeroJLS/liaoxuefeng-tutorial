5423170d39841014e64c1277dde2faeb0cadc98a
jiangls <1257892551@qq.com>
2020-07-07 21:53:40 +0800

    feat(java基础):FunctionalProgrammingTest中加注释

--- src/com/javabase/FunctionalProgrammingTest.java
+++ src/com/javabase/FunctionalProgrammingTest.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class FunctionalProgrammingTest {
     public static void main(String[] args) {
+        
         List<String> list = Arrays.asList(new String[]{"Apple", "Banana", "Orange"});
-        String[] array = list.stream().toArray(String[]::new);
+        String[] array = list.stream().toArray(String[]::new);// 调用String[]构造方法
         System.out.println(Arrays.toString(array));
     }
 }
